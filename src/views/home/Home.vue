ed0b0031560d33f4fa24443ffac096b7337c456d
SmartFang6 <1270358909@qq.com>
2020-03-26 00:26:01 +0800

    1111

---
+++ src/views/home/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="wrapper">
+    <home-headers></home-headers>
+    <home-swiper />
+    <home-icons />
+  </div>
+</template>
+
+<script>
+import HomeHeaders from './components/Header'
+import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
+export default {
+  components:{
+    HomeHeaders,
+    HomeSwiper,
+    HomeIcons
+  },
+  props:{},
+  data(){
+    return {
+    }
+  },
+  computed:{},
+  created(){},
+  methods:{},
+}
+</script>
+<style  scoped>
+</style>
