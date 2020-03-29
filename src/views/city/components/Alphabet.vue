3418567aefd8c7aeeaf6b16830a89137a870ce53
SmartFang6 <1270358909@qq.com>
2020-03-29 10:26:39 +0800

    city-list

---
+++ src/views/city/components/Alphabet.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="list">
+    <div class="item">a</div>
+    <div class="item">a</div>
+    <div class="item">a</div>
+    <div class="item">a</div>
+    <div class="item">a</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Alphabet',
+  components:{},
+  props:{},
+  data(){
+    return {
+    }
+  },
+  computed:{},
+  created(){},
+  methods:{},
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+.list
+  position absolute
+  top 1.58rem
+  right 0
+  bottom 0
+  width .4rem
+  display  flex
+  flex-direction column
+  justify-content center
+  .item
+    text-align center
+    line-height .4rem
+    color $bgColor
+</style>
